5ac7bf03237bb01c6fb0d5d8d95d8d4fe39cd933
Harry Fleminger <harry.fleminger@hotmail.com>
2023-01-10 11:05:29 +0000

    Started carosuel for project pop up with all JS, HTML and CSS finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// menu managment
 const menuBar = document.querySelector('.fa-solid.fa-bars.fa-xl')
 const menuX = document.querySelector('.fa-solid.fa-xmark.fa-2xl')
 const mobileNav = document.querySelector('.navbar-mobile')
@@ -21,3 +22,24 @@ mobileCover.addEventListener("click", (event) => {
   mobileNav.classList.remove('show-navbar-mobile')
   mobileCover.classList.remove('show-mobile-cover')
 });
+
+// Carousel
+var slides = document.querySelectorAll('.slide');
+var currentSlide = 0;
+setInterval(nextSlide, 5000);
+
+nextSlide()
+
+function nextSlide() {
+  goToSlide(currentSlide+1);
+}
+
+function prevSlide() {
+  goToSlide(currentSlide-1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].className = 'slide active';
+}
