ab193192f8369a7ef689fa4393598152298697de
Harry Fleminger <harry.fleminger@hotmail.com>
2023-01-17 17:02:19 +0000

    Added Gibson's Bakery and Sellebrity to my projects. Tidied up CSS and JS

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const mobileCover = document.querySelector('.mobile-cover')
 // Project popups variables
 const travelPlan = document.querySelector('#travel-plan')
 const travelPlanContent = document.querySelector('#travel-plan-content')
-const popUpClose = document.querySelector('.close-pop')
+const popUpClose = document.querySelectorAll('.close-pop')
 const eatMyWay = document.querySelector('#eat-my-way')
 const eatMyWayContent = document.querySelector('#eat-my-way-content')
+const gibsonsBakery = document.querySelector('#gibsons-bakery')
+const gibsonsBakeryContent = document.querySelector('#gibsons-bakery-content')
+const sellebrity = document.querySelector('#sellebrity')
+const sellebrityContent = document.querySelector('#sellebrity-content')
 // Carousel variables
 // var slides = document.querySelectorAll('.slide');
 // var currentSlide = 0;
@@ -33,49 +37,38 @@ mobileCover.addEventListener("click", (event) => {
   mobileCover.classList.remove('show-mobile-cover')
   travelPlanContent.classList.remove('modal-content-show')
   eatMyWayContent.classList.remove('modal-content-show')
-  carousel1.goToSlide(1);
+  gibsonsBakeryContent.classList.remove('modal-content-show')
+  sellebrityContent.classList.remove('modal-content-show')
+  carousel1.goToSlide(0);
   carousel1.stopSlideTransition()
   carousel2.goToSlide(0);
-  carousel1.stopSlideTransition()
-});
-popUpClose.addEventListener("click", (event) => {
-  mobileNav.classList.remove('show-navbar-mobile')
-  mobileCover.classList.remove('show-mobile-cover')
-  travelPlanContent.classList.remove('modal-content-show')
-  eatMyWayContent.classList.remove('modal-content-show')
+  carousel2.stopSlideTransition()
+  carousel3.goToSlide(0);
+  carousel3.stopSlideTransition()
+  carousel4.goToSlide(0);
+  carousel4.stopSlideTransition()
 });
 
-
-
-// Carousel JS
-// setInterval(nextSlide, 5000);
-
-// nextSlide()
-
-// function nextSlide() {
-//   goToSlide(currentSlide+1);
-// }
-
-// function prevSlide() {
-//   goToSlide(currentSlide-1);
-// }
-
-// function goToSlide(n) {
-//   slides[currentSlide].className = 'slide';
-//   currentSlide = (n+slides.length)%slides.length;
-//   slides[currentSlide].className = 'slide active';
-// }
+for (let i = 0; i < popUpClose.length; i++) {
+  popUpClose[i].addEventListener("click", (event) => {
+    mobileNav.classList.remove('show-navbar-mobile')
+    mobileCover.classList.remove('show-mobile-cover')
+    travelPlanContent.classList.remove('modal-content-show')
+    eatMyWayContent.classList.remove('modal-content-show')
+  });
+}
 
 class Carousel {
   constructor(element) {
     this.slides = element.querySelectorAll('.slide');
     this.currentSlide = 0;
     this.intervalId = setInterval(this.nextSlide.bind(this), 5000);
-    this.goToSlide(1);
+    this.goToSlide(0);
+    this.stopSlideTransition();
   }
 
   nextSlide() {
-    clearInterval(this.intervalId);
+    this.stopSlideTransition()
     this.intervalId = setInterval(this.nextSlide.bind(this), 5000);
     this.goToSlide(this.currentSlide+1);
   }
@@ -84,11 +77,8 @@ class Carousel {
     clearInterval(this.intervalId);
   }
 
-  clearTheInterval() {
-    clearInterval(this.intervalId);
-  }
   prevSlide() {
-    clearInterval(this.intervalId);
+    this.stopSlideTransition()
     this.intervalId = setInterval(this.nextSlide.bind(this), 5000);
     this.goToSlide(this.currentSlide-1);
   }
@@ -102,14 +92,26 @@ class Carousel {
 
 const carousel1 = new Carousel(document.getElementById('carousel1'));
 const carousel2 = new Carousel(document.getElementById('carousel2'));
+const carousel3 = new Carousel(document.getElementById('carousel3'));
+const carousel4 = new Carousel(document.getElementById('carousel4'));
 // Project popups JS
 travelPlan.addEventListener("click", (event) => {
-  carousel1.clearTheInterval()
+  carousel1.stopSlideTransition()
   travelPlanContent.classList.add('modal-content-show')
   mobileCover.classList.add('show-mobile-cover')
 });
 eatMyWay.addEventListener("click", (event) => {
-  carousel2.clearTheInterval()
+  carousel2.stopSlideTransition()
   eatMyWayContent.classList.add('modal-content-show')
+  mobileCover.classList.add('show-mobile-cover')
+});
+gibsonsBakery.addEventListener("click", (event) => {
+  carousel3.stopSlideTransition()
+  gibsonsBakeryContent.classList.add('modal-content-show')
+  mobileCover.classList.add('show-mobile-cover')
+});
+sellebrity.addEventListener("click", (event) => {
+  carousel4.stopSlideTransition()
+  sellebrityContent.classList.add('modal-content-show')
   mobileCover.classList.add('show-mobile-cover')
 });
